bf3c3dbf943c9baeb8cac67bfde7777b1ed544ce
Iskandar-tulkinov <iscandardev@gmail.com>
2024-01-27 11:59:19 +0500

    feat:set up project, and create being sponsor form

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# css-starter
-With it you can immediately start your project that has CSS and JS with theme-toggler.
+# Metsenat
---
+++ components/tab/tab.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// function openTab(e, tabName) {
+
+//    // const tabContents = document.querySelectorAll(".tab-content");
+//    const tabLinks = document.querySelectorAll(".tab-link");
+
+//    for (let i = 0; i < tabLinks.length; i++) {
+//       tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+//    }
+
+//    const currentTab = document.getElementById(tabName);
+//    currentTab.style.display = "block";
+//    e.currentTarget.className += " active";
+
+//    // Hide or show specific inputs based on the tabName
+//   //  const legalEntityEl = currentTab.querySelector(".legal-entity-el");
+
+//   //  if (tabName === "LegalEntity") {
+//   //     legalEntityEl.style.display = "block";
+
+//   //     document.querySelector(".org-name").value=''
+//   //  } else {
+//   //     legalEntityEl.style.display = "none";
+//   //  }
+// }
+
+// document.getElementById("defaultOpen").click();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const checkbox = document.getElementById('LegalEntity');
+  const legalEntityEl = document.querySelector('.legal-entity-el');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      legalEntityEl.style.display = 'block';
+    } else {
+      legalEntityEl.style.display = 'none';
+    }
+  });
+});
+
+const otherSumInput = document.querySelector(".other-sum");
+
+document.querySelector(".other-sum-label").addEventListener("click", () => {
+   otherSumInput.style.display = "block";
+});
+
+document.querySelector(".form").addEventListener("submit", (e) => {
+   e.preventDefault();
+});
