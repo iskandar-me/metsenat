4bf4dddec0015d7b4df6a6f0466c81100a408edd
Iskandar-tulkinov <iscandardev@gmail.com>
2024-01-30 13:16:27 +0500

    feat: create filter-sponsors modal and add-student modal

--- components/tab/tab.js
+++ components/tab/tab.js
@@ -1,31 +1,5 @@
 "use strict";
 
-// function openTab(e, tabName) {
-
-//    // const tabContents = document.querySelectorAll(".tab-content");
-//    const tabLinks = document.querySelectorAll(".tab-link");
-
-//    for (let i = 0; i < tabLinks.length; i++) {
-//       tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-//    }
-
-//    const currentTab = document.getElementById(tabName);
-//    currentTab.style.display = "block";
-//    e.currentTarget.className += " active";
-
-//    // Hide or show specific inputs based on the tabName
-//   //  const legalEntityEl = currentTab.querySelector(".legal-entity-el");
-
-//   //  if (tabName === "LegalEntity") {
-//   //     legalEntityEl.style.display = "block";
-
-//   //     document.querySelector(".org-name").value=''
-//   //  } else {
-//   //     legalEntityEl.style.display = "none";
-//   //  }
-// }
-
-// document.getElementById("defaultOpen").click();
 
 
 document.addEventListener('DOMContentLoaded', function () {
--- pages/admin-panel/admin-panel.js
+++ pages/admin-panel/admin-panel.js
@@ -1,35 +1,86 @@
 "use strict";
+const tablinks = document.querySelectorAll(".tablink");
 function openTab(e, tabname) {
-   const tablinks = document.querySelectorAll(".tablinks");
    const tabcontents = document.querySelectorAll(".tab-content");
-   let i;
-   for (i = 0; i < tabcontents.length; i++) {
-      tabcontents[i].style.display = "none";
-   }
-   for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active","");
-   }
 
+   // Hide all tab contents
+   tabcontents.forEach((tabcontent) => {
+      tabcontent.style.display = "none";
+   });
+
+   // Remove the "active" class from all tab links
+   tablinks.forEach((tablink) => {
+      tablink.classList.remove("active");
+   });
+
+   // Display the clicked tab content and add the "active" class to the clicked tab link
    document.getElementById(tabname).style.display = "block";
-   e.currentTarget.className += " active";
+   console.log("tab", tabname);
+   e.currentTarget.classList.add("active");
 }
-
 document.getElementById("defaultOpen").click();
 
-const canvas = document.getElementById("my-canvas");
-const ctx = canvas.getContext("2d");
+const filterBtn = document.querySelector(".filter-wrapper");
+const filterSponsorsForm = document.querySelector(".filter-sponsors-form");
+const overlay = document.querySelector(".overlay");
+const modals = document.querySelectorAll(".modal");
+const btns = document.querySelectorAll(".btn");
+const closeBtn = document.querySelector(".close-btn");
 
-ctx.fillStyle = "#feaada";
-ctx.fillRect(0, 0, 250, 155);
-ctx.moveTo(10, 10);
+filterBtn.addEventListener("click", () => {
+   console.log("filter");
+   filterSponsorsForm.classList.remove("hidden");
+   overlay.classList.remove("hidden");
+   document.body.style.overflow = "hidden";
+});
 
-ctx.lineTo(10,120);
-ctx.lineWidth =15
-canvas.lineCap = "square";
-ctx.strokeStyle='green';
-ctx.stroke()
-ctx.beginPath();
-ctx.arc(120,70,50,0,6.28)
-ctx.strokeStyle='#fefefe'
-ctx.lineWidth =5
-ctx.stroke()
+overlay.addEventListener("click", closeModal);
+
+closeBtn.addEventListener("click", closeModal);
+document.addEventListener("keydown", (event) => {
+   if (event.key === "Escape") {
+      closeModal();
+   }
+});
+function closeModal() {
+   modals.forEach((modal) => {
+      modal.classList.add("hidden");
+   });
+   overlay.classList.add("hidden");
+   document.body.style.overflow = "auto";
+}
+
+btns.forEach((btn) => {
+   btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("btn is clicked");
+   });
+});
+
+const eyeIcons = document.querySelectorAll(".eye-icon");
+
+eyeIcons.forEach((eyeIcon) => {
+   eyeIcon.addEventListener("click", (e) => {
+      console.log('eyeIcon is clicked');
+      console.log( e.target);
+   });
+});
+
+
+// const canvas = document.getElementById("my-canvas");
+// const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = "#feaada";
+// ctx.fillRect(0, 0, 250, 155);
+// ctx.moveTo(10, 10);
+
+// ctx.lineTo(10,120);
+// ctx.lineWidth =15
+// canvas.lineCap = "square";
+// ctx.strokeStyle='green';
+// ctx.stroke()
+// ctx.beginPath();
+// ctx.arc(120,70,50,0,6.28)
+// ctx.strokeStyle='#fefefe'
+// ctx.lineWidth =5
+// ctx.stroke()
