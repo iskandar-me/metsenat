7c928ed0baee42edba9c35a61c2db91ec8b170ac
Iskandar-tulkinov <iscandardev@gmail.com>
2024-02-01 19:23:58 +0500

    fix: var naming

--- js/script.js
+++ js/script.js
@@ -1,46 +1,41 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', function () {
-  const checkbox = document.getElementById('LegalEntity');
-  const legalEntityEl = document.querySelector('.legal-entity-el');
+document.addEventListener("DOMContentLoaded", function () {
+   const checkbox = document.getElementById("LegalEntity");
+   const legalEntityEl = document.querySelector(".legal-entity-el");
 
-  checkbox.addEventListener('change', function () {
-    if (checkbox.checked) {
-      legalEntityEl.classList.remove("hidden")
-    } else {
-      legalEntityEl.classList.add("hidden")
-    }
-  });
+   checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+         legalEntityEl.classList.remove("hidden");
+      } else {
+         legalEntityEl.classList.add("hidden");
+      }
+   });
 });
- radioLabels = document.querySelectorAll(".radio-label");
+const radioLabels = document.querySelectorAll(".radio-label");
 
 radioLabels.forEach((label) => {
-  label.addEventListener("click", function () {
-     // Uncheck all radio buttons
-     radioLabels.forEach((label) => {
-        label.parentNode.classList.remove("checked");
-        otherSumInput.style.display = "none";
-     });
+   label.addEventListener("click", function () {
+      // Uncheck all radio buttons
+      radioLabels.forEach((label) => {
+         label.parentNode.classList.remove("checked");
+         otherSumInput.style.display = "none";
+      });
 
-     // Check the clicked radio button
-     this.parentNode.classList.add("checked");
-  });
+      // Check the clicked radio button
+      this.parentNode.classList.add("checked");
+   });
 });
 
 const sponsorshipSubmitBtn = document.querySelector(".sponsorship-submit");
 sponsorshipSubmitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector(".data-sent-section").style.display = "flex";
-  document.querySelector(".application-section").style.display = "none";
-  // const sponsorshipInputs = document.querySelectorAll(".input");
-  // sponsorshipInputs.forEach((input) => {
-  //    if (!input.value.trim().length) {
-  //       input.classList.add("error");
-  //       setTimeout(() => {
-  //          input.classList.remove("error");
-  //       }, 2000);
-  //    }else{
-  //       input.classList.remove("error");
-  //       console.log(input.value);
-  //    }
-  // });
+   e.preventDefault();
+   document.querySelector(".data-sent-section").style.display = "flex";
+   document.querySelector(".application-section").style.display = "none";
+   console.log("clicked");
+});
+sponsorshipSubmitBtn.addEventListener("submit", (e) => {
+   e.preventDefault();
+   document.querySelector(".data-sent-section").style.display = "flex";
+   document.querySelector(".application-section").style.display = "none";
+   console.log("submit clicked");
 });
